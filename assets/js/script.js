cff4525e752c25f58000db3bfeca2217fd1ca057
fikri47 <fadillahfikri48@gmail.com>
2023-02-22 12:56:59 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+const nav = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const positionWindow = window.scrollY > 0;
+  nav.classList.toggle("active", positionWindow);
+});
+
+function display_c() {
+    var refresh = 1000; // Refresh rate in milli seconds
+    mytime = setTimeout("display_ct()", refresh);
+  }
+  
+  function display_ct() {
+    var x = new Date();
+    document.getElementById("current_time").innerHTML = x;
+    display_c();
+  }
+
+  function validateForm() {
+    var name = document.forms["message-form"]["nama"].value;
+    var date = document.forms["message-form"]["birth-date"].value;
+    var gender = document.forms["message-form"]["gender"].value;
+    var messages = document.forms["message-form"]["pesan"].value;
+
+    if (name == "" || date == "" || gender == "" || messages == "") {
+      alert("Input Tidak Boleh Kosong");
+      return false;
+    }
+    
+    document.getElementById("inputNama").innerText= name;
+    document.getElementById("welcome-nama").innerText= name;
+    document.getElementById("inputDate").innerText = date;
+    document.getElementById("inputGender").innerText = gender;
+    document.getElementById("inputPesan").innerText = messages;
+    return false
+
+  }
+
+  btnSubmit.addEventListener("click", validateForm);
+
+
